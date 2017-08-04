33c5cb3ad07c32322a10bc1771c6f539c1f20994
Maik Messerschmidt <maik.messerschmidt@gmx.net>
2017-08-04 21:20:57 +0200

    Changed sys version check to be more general.

--- mtoolbox/instancelog.py
+++ mtoolbox/instancelog.py
@@ -34,7 +34,7 @@ In this example ``MyClass1`` objects will not be logged, while
 
 import sys
 
-if sys.version_info.major == 2:
+if sys.version_info[0] == 2:
     import __builtin__ as builtins
 else:
     import builtins
